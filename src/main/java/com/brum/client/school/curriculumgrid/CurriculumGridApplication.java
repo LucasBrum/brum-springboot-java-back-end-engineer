cdd8b40ca13402012e37b3dc58828ea48a2744e9
Lucas Brum <brum.lucas@icloud.com>
2020-11-26 19:18:41 -0300

    Creating Subject Controller

--- src/main/java/com/brum/client/school/curriculumgrid/CurriculumGridApplication.java
+++ src/main/java/com/brum/client/school/curriculumgrid/CurriculumGridApplication.java
@@ -2,9 +2,16 @@ package com.brum.client.school.curriculumgrid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CurriculumGridApplication {
+public class CurriculumGridApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CurriculumGridApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurriculumGridApplication.class, args);
---
+++ src/main/java/com/brum/client/school/curriculumgrid/controller/SubjectController.java
@@ -0,0 +1,18 @@
+package com.brum.client.school.curriculumgrid.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/subject")
+public class SubjectController {
+
+	@GetMapping
+	public ResponseEntity<String> helloWorldRest() {
+		return ResponseEntity.status(HttpStatus.OK).body("Olá Mundo Rest!!!");
+	}
+	
+}
