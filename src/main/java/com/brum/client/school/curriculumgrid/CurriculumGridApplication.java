8266fda7cc2417c32d2e4e505f62819973695b04
Lucas Brum <brum.lucas@icloud.com>
2020-11-26 09:18:19 -0300

    Create project

---
+++ src/main/java/com/brum/client/school/curriculumgrid/CurriculumGridApplication.java
@@ -0,0 +1,13 @@
+package com.brum.client.school.curriculumgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumGridApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brum/client/school/curriculumgrid/CurriculumGridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brum.client.school.curriculumgrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumGridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
