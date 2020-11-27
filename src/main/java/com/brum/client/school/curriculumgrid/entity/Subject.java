2368d77575c96a3571f6e7a83da188de648bc48d
Lucas Brum <brum.lucas@icloud.com>
2020-11-27 18:35:25 -0300

    Creating Entity Subject

---
+++ src/main/java/com/brum/client/school/curriculumgrid/entity/Subject.java
@@ -0,0 +1,44 @@
+package com.brum.client.school.curriculumgrid.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Subject implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@JsonInclude(Include.NON_NULL)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@JsonInclude(Include.NON_EMPTY)
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "hours")
+	private int hours;
+	
+	@Column(name = "code")
+	private String code;
+	
+	@Column(name = "frequency")
+	private int frequency;
+	
+	
+}
