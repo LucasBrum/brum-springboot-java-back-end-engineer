5a0bb58ec37118416516c7072b289a2c1bc96a48
Lucas Brum <brum.lucas@icloud.com>
2021-04-02 21:08:42 -0300

    Create PasswordEconder Bean

--- src/main/java/com/brum/client/school/curriculumgrid/config/WebSecurityConfig.java
+++ src/main/java/com/brum/client/school/curriculumgrid/config/WebSecurityConfig.java
@@ -1,15 +1,22 @@
 package com.brum.client.school.curriculumgrid.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public PasswordEncoder encorder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Autowired
 	public void globalConfiguration(AuthenticationManagerBuilder auth) throws Exception {
